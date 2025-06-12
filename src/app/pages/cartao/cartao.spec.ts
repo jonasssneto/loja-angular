e4a200a21d5ce6c75d195487aa11716400a42e66
Jonas Neto <nogueiraneto.jonas@gmail.com>
2025-06-11 23:24:05 -0300

    chore: setup inicial

---
+++ src/app/pages/cartao/cartao.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Cartao } from './cartao';
+
+describe('Cartao', () => {
+  let component: Cartao;
+  let fixture: ComponentFixture<Cartao>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Cartao]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Cartao);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
